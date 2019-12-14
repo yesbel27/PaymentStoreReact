b89af3453323c6efaa127776ce70758a7f660dba
richard pumapillo <richardpumac@gmail.com>
2019-12-14 02:24:17 -0500

    correccion d eerror

--- src/redux/actions/productActions.js
+++ src/redux/actions/productActions.js
@@ -11,12 +11,9 @@ export function getProducts() {
   
   return(dispatch)=>{   
     dispatch(beginApiCall());
-    //Axios.get('https://my-json-server.typicode.com/richardpuma/db_data/products')
-<<<<<<< HEAD
+
     return Axios.get("http://3.15.174.163/api/products",{headers:{"Access-Control-Allow-Origin": "*"}})
-=======
-    return Axios.get("http://localhost:8090/api/products",{headers:{"Access-Control-Allow-Origin": "*"}})
->>>>>>> a6636f2c3c0f0169c1081e6358d9b98c6457b724
+
     .then(result=>{
       dispatch(getProductSuccess(result.data));
     }).catch(error=>{
